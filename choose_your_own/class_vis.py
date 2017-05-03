0f0fa32b023c0f43ce4f6bf2b121cdec7d7ec6e7
Leo Zhou <glasslion@gmail.com>
2017-05-03 21:50:31 +0800

    homework for choose your own model

--- choose_your_own/class_vis.py
+++ choose_your_own/class_vis.py
@@ -32,8 +32,7 @@ def prettyPicture(clf, X_test, y_test):
     plt.legend()
     plt.xlabel("bumpiness")
     plt.ylabel("grade")
-
-    plt.savefig("test.png")
+    plt.show()
 
 import base64
 import json
